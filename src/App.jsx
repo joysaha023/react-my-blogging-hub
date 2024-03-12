7dd54bfed0d7ac5909a9f44eac9cef943ce8cf4a
Joy <github.joysaha@gmail.com>
2024-03-12 20:40:24 +0600

    first commit and basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='bg-purple-400 text-6xl font-extrabold'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
