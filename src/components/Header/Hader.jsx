3b48164ed04c8229792d9da88e67a226a2645774
Joy <github.joysaha@gmail.com>
2024-03-12 22:50:21 +0600

    navbar design done

--- src/components/Header/Hader.jsx
+++ src/components/Header/Hader.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='bg-purple-400 text-6xl font-extrabold'>React</h1>
+        <div className='flex justify-between items-center border-b-2 mb-1'>
+            <h1 className='text-2xl lg:text-4xl font-bold '>Knowledge Cafe</h1>
+            <img className='mb-1' src={profile} alt="" />
         </div>
+        
     );
 };
 
